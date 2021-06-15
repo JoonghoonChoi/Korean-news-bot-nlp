72be3f1854e09e0ef02244956e77fa583914768c
joonghoonc <joonghoon.choi@icloud.com>
2021-06-15 18:27:14 +0900

    fit-on-colab-output

--- result.py
+++ result.py
@@ -13,17 +13,18 @@ def show_result(model, summarizer, texts,
         코랩 노트북에서 print()하면 텍스트가 1열로 나와서 가독성이 안좋음
         텍스트 길이 80을 기준으로 잘라서 출력해주는 함수 구현
         '''
-        # if len(text) > 80:    
-        #     start = 0
-        #     end = 80
-        #     for _ in range(1, int(len(text) / 80)+2):
-        #         print(text[start:end])
-        #         start = end
-        #         end += 80
-        # else:
-        #     print(text)
+        if len(text) > 100:    
+            start = 0
+            end = 100
+            for _ in range(1, int(len(text) / 80)+2):
+                print(text[start:end])
+                start = end
+                end += 80
+        else:
+            print(text)
+            
         '''쥬피터 노트북에서는 전체 출력하는게 가독성이 좋음'''
-        print(text)
+#         print(text)
 
     for i in range(len(texts)):
         print('[Original:]')
